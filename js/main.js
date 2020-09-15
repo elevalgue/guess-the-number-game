fc5ed83cf3bfc058454d7a8071739a54d7c6bced
elevalgue <elena.valenzuela.guerrero@gmail.com>
2020-09-15 17:08:54 +0200

    Add attempts

--- js/main.js
+++ js/main.js
@@ -7,10 +7,7 @@ console.log('Mi número aleatorio es: ', randomNumber);
 
 // show info
 const clueMessage = document.querySelector('.js-clue-message');
-const attemptsMessage = document.querySelector('.js-attemps');
-
-// attempts
-let attemps = 0;
+const attemptsNumber = document.querySelector('.js-attemptsNumber');
 
 // FUNCTIONS
 
@@ -23,39 +20,40 @@ function getRandomNumber(max) {
 
 const submitButton = document.querySelector('.js-submitButton');
 const resetButton = document.querySelector('.js-reset-button');
-const inputNumber = document.querySelector('.js-number');
+const userNumber = document.querySelector('.js-number');
 let clue = document.querySelector('.js-clue');
-
-console.log(number.value);
 
 function handleSubmitButton(event) {
   event.preventDefault();
-  let numberValue = parseInt(number.value);
-  if (numberValue < 1 || numberValue > 100) {
+  updateClue();
+  updateAttempts(event);
+}
+
+function updateClue() {
+  let userNumber = parseInt(number.value);
+  if (userNumber > 100 || userNumber < 1) {
     clue.innerHTML = 'El número debe ser mayor que 0 y menor que 100';
-  } else if (numberValue < randomNumber) {
+  } else if (userNumber < randomNumber) {
     clue.innerHTML = 'Demasiado bajo ❄️';
-  } else if (numberValue > randomNumber) {
+  } else if (userNumber > randomNumber) {
     clue.innerHTML = 'Es mayor 🔥';
-  } else if (numberValue === randomNumber) {
+  } else if (userNumber === randomNumber) {
     clue.innerHTML = '🏆 ¡Has ganado campeona! 🏆';
   } else {
     clue.innerHTML = 'errorrrrrr';
   }
 }
 
-submitButton.addEventListener('click', handleSubmitButton);
-//
-// function checkNumber() {
-//   attemps++;
-//   const userNumber = userInput;
-//   console.log(`Número elegido ${userNumber}`);
+// attempts
+let attempts = 0;
 
-//   let attemptsTest = (attemps += 1);
-//   attemptsMessages.innerHTML = `${attemptsTest} intentos`;
-//   console.log(attemptsTest);
+function updateAttempts(event) {
+  attempts++;
+  attemptsNumber.innerHTML = attempts;
+}
 
-//
-// listener
+console.log('Número de intentos', attempts);
 
-// button.addEventListener('click', userNumber);
+// listeners
+
+submitButton.addEventListener('click', userNumber);
